db9aa5079fb59326d8f9e655cf2c7549f892f504
tomoya.hirano <tomoya.hirano@dena.com>
2020-12-02 22:09:17 +0900

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by Tomoya Hirano on 2020/12/02.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
